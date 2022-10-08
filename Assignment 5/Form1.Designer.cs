48e276117fe9350c2fd8a7d01321fedfdb61e49f
NikitaEeTu <nikita71140@gmail.com>
2022-10-08 07:45:51 +0300

    Добавьте файлы проекта.

---
+++ Assignment 5/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Assignment_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(267, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(401, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(267, 32);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(209, 61);
+            this.textBox2.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private TextBox textBox2;
+        private ContextMenuStrip contextMenuStrip1;
+    }
+}
---
+++ Assignment 5/Form1.cs
@@ -0,0 +1,61 @@
+﻿namespace Assignment_5
+{
+    public partial class Form1 : Form
+    {
+        private int numberOfGuesses = 0;
+        private int checkTheNumber(String number)
+        {
+            try {
+                int checkedNumber = Int16.Parse(number);
+                if(checkedNumber >= 0 && checkedNumber <= 100)
+                {
+                    return checkedNumber;
+                }
+                else
+                {
+                    textBox2.Text = "Your number is not in the right range";
+                    return -1;
+                }
+            }
+            catch
+            {
+                textBox2.Text = "You haven't written a number";
+                return -1;
+            }
+        }
+
+        private String checkUserInputWithRandomNumber(int userNubmer, int randomNumber)
+        {
+            if(userNubmer > randomNumber)
+            {
+                return String.Format("Your number is greater than the computer guessed number{0}", "📈");
+            }
+            else if(userNubmer < randomNumber)
+            {
+                return String.Format("Your number is lower than the computer guessed number{0}", "📉");
+            }
+            else
+            {
+                numberOfGuesses += 1;
+                return String.Format("Congrutulations! Your number is the same as computer's one{0}. Your number of gueses was {1}", "🥇", numberOfGuesses);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String userChoiceNumber = textBox1.Text;
+            int checkednumber = checkTheNumber(userChoiceNumber);
+            if (checkednumber != -1)
+            {
+                Random r = new Random();
+                int computerRandomNuber = r.Next(0, 100);
+
+                textBox2.Text = checkUserInputWithRandomNumber(checkednumber, computerRandomNuber);
+            }
+        }
+    }
+}
